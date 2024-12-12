9e3f3c53e84572b9ca60e48daf2be1a7384324d6
nek0us <1130131059@qq.com>
2024-12-12 11:28:02 +0800

    update js,2024.12.12

--- ChatGPTWeb/load.js
+++ ChatGPTWeb/load.js
@@ -1,5 +1,5 @@
 
-import('https://cdn.oaistatic.com/assets/dh0yl0m9q337gmci.js').then((module) => {
+import('https://cdn.oaistatic.com/assets/cuau6nn9sjz8q54j.js').then((module) => { 
     window._chatp_old = module.cJ;//bT;
     window._ark = module.cJ;//?bS;
     window._chatp = module.cI;//bR;
@@ -7,9 +7,7 @@ import('https://cdn.oaistatic.com/assets/dh0yl0m9q337gmci.js').then((module) => 
     window._wss = module.J;//K;
 }).catch(error => console.error("Error loading dh0yl0m9q337gmci.js:", error));
 
-import('https://cdn.oaistatic.com/assets/ktiwgucld5a8s55m.js').then((module) => {
+import('https://cdn.oaistatic.com/assets/ebwx7yj0baonheef.js').then((module) => { 
     window._turnstile = module.ix;//ce;
     window._proof = module.jt;//Jg;
 }).catch(error => console.error("Error loading ktiwgucld5a8s55m.js:", error));
-
-
